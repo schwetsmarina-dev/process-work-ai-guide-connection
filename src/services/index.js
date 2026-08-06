2ac59daa16e26446ff703c783ce24a48c53c3420
schwetsmarina-dev <schwets.marina@gmail.com>
2026-08-06 23:11:26 +0100

    export Talvira services

---
+++ src/services/index.js
@@ -0,0 +1,6 @@
+export { usersService } from "./users";
+export { sessionsService } from "./sessions";
+export { messagesService } from "./messages";
+export { memoryService } from "./memory";
+export { aiService } from "./ai";
+export { paymentsService } from "./payments";
